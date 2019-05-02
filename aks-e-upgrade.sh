a0cfe753c3b7fbd79d4244551185e9fb743a4025
Jason De Lorme <jasondel@microsoft.com>
2019-05-02 17:47:42 -0400

    Removing errant script.

--- aks-e-upgrade.sh
+++ aks-e-upgrade.sh
@@ -6,8 +6,3 @@ aks-engine upgrade --force --subscription-id 57052a80-f184-4850-9b7b-26d0db87a70
     --api-model ./_output/jasondel-akse/apimodel.json \
     --client-id="XXX" \
     --client-secret="XXX"
-
-
-git filter-branch --force --index-filter \
-  'git rm --cached --ignore-unmatch aks-e-upgrade.sh' \
-  --prune-empty --tag-name-filter cat -- --all
